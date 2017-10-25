b4d6cf145a6be09ca1f5a6bb9d9e1a26aa46ba6c
Arya Atighehchian <arya.atighehchian.13@cnu.edu>
2017-10-25 09:34:45 -0400

    practice tests

--- src/test/java/coveros/getting_started/SampleTests.java
+++ src/test/java/coveros/getting_started/SampleTests.java
@@ -1,6 +1,7 @@
 package coveros.getting_started;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import org.openqa.selenium.Keys;
 import org.testng.ITestContext;
@@ -11,6 +12,7 @@ import org.testng.annotations.Test;
 import com.coveros.selenified.Locator;
 import com.coveros.selenified.Selenified;
 import com.coveros.selenified.application.App;
+import com.coveros.selenified.element.Element;
 
 public class SampleTests extends Selenified {
 
@@ -53,4 +55,67 @@ public class SampleTests extends Selenified {
         // verify no issues
         finish();
     }
+    
+    @Test
+    public void testAboutButton() throws Exception {
+    	App app = this.apps.get();
+    	app.newElement(Locator.LINKTEXT,"About").click();
+    	//app.azzert().urlEquals("https://www.google.com/intl/en/about/?fg=1&utm_source=google.com&utm_medium=referral&utl_campaign=hp-header");
+    	app.azzert().titleEquals("Our Latest | Google");
+    	finish();
+    }
+    
+    
+    @Test
+    public void testStoreButton() throws Exception {
+    	App app = this.apps.get();
+    	app.newElement(Locator.LINKTEXT, "Store").click();
+    	//app.azzert().urlEquals("https://store.google.com/?utm_source=hp_header&utm_medium=google_oo&utm_campaign=GS100042");
+    	app.azzert().titleEquals("Shop Pixel, Chromecast, Google Home and more at Google Store");
+    	finish();
+    }
+    
+    @Test
+    public void testYoutube() throws Exception {
+    	App app = this.apps.get();
+    	app.newElement(Locator.CLASSNAME, "_XIi").click();
+    	//app.azzert().urlEquals("https://tv.youtube.com/welcome/?utm_source=hpp&utm_medium=hybrid&utm_campaign=ws_1024");
+    	app.azzert().titleEquals("YouTube TV - Watch & DVR Live Sports, Shows & News");
+    	finish();
+    }
+    
+    @Test
+    public void testDropDown() throws Exception {
+    	App app = this.apps.get();
+    	app.newElement(Locator.LINKTEXT, "Settings").click();
+    	app.newElement(Locator.LINKTEXT, "Search Help").click();
+    	//app.azzert().urlEquals("https://support.google.com/websearch/?visit_id=1-636444718668472423-981266810&hl=en&rd=2#topic=3378866");
+    	app.azzert().textPresent("Welcome to the Google Search Help Center");
+    	finish();
+    }
+    @Test
+    public void testHover() throws Exception {
+    	App app = this.apps.get();
+    	app.newElement(Locator.ID, "gbqfbb").hover();
+    	
+    	HashSet<String> lucky = new HashSet<String>();
+    	
+    	lucky.add("I'm Feeling Doodley");
+    	lucky.add("I'm Feeling Artisitc");
+    	lucky.add("I'm Feeling Hungry");
+    	lucky.add("I'm Feeling Puzzled");
+    	lucky.add("I'm Feeling Trendy");
+    	lucky.add("I'm Feeling Stellar");
+    	lucky.add("I'm Feeling Playful");
+    	lucky.add("I'm Feeling Wonderful");
+    	lucky.add("I'm Feeling Generous");
+    	lucky.add("I'm Feeling Curious");
+    	
+    	app.newElement(Locator.ID, "gbqfba");
+    	app.azzert().textPresent();
+    	
+    	finish();
+    	
+    }
+    
 }
